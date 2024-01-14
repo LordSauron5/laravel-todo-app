cee96ec99146a2a1e0e924672748a9b254cfd82c
Tyron Aricum <tyronaricum1@gmail.com>
2024-01-14 18:48:25 +0200

    update: add toggle state

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,11 @@
 
 @section('title', "The list of tasks")
 
-@section('content')    
+@section('content')   
+    <div>
+        <a href="{{ route('tasks.create') }}">Create New Task</a>
+    </div>
+
     @forelse ($tasks as $task)
         <div>
             <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
