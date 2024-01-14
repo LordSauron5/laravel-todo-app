e65916899d32c01cd3d4498384f35764033ca68b
Tyron Aricum <tyronaricum1@gmail.com>
2024-01-14 15:50:46 +0200

    update: create edit functionality

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', "Add Task")
+@section('title', "Edit Task")
 
 @section('styles')
     <style>
@@ -12,11 +12,12 @@
 @endsection
 
 @section('content')
-    <form action="{{ route('tasks.store') }}" method="post">
+    <form action="{{ route('tasks.update', ['id' => $task->id]) }}" method="post">
         @csrf
+        @method('PUT')
         <div>
             <label for="title">Title</label>
-            <input type="text" name="title" id="title">
+            <input type="text" name="title" id="title" value="{{ $task->title }}">
             @error('title')
                 <p class="error-message">{{ $message }}</p>
             @enderror
@@ -24,7 +25,7 @@
         
         <div>
             <label for="description">Description</label>
-            <textarea type="description" name="description" id="description" rows="5"></textarea>
+            <textarea type="description" name="description" id="description" rows="5">{{ $task->description }}</textarea>
             @error('description')
             <p class="error-message">{{ $message }}</p>
             @enderror
@@ -32,14 +33,14 @@
 
         <div>
             <label for="long_description">Description</label>
-            <textarea type="long_description" name="long_description" id="long_description" rows="10"></textarea>
+            <textarea type="long_description" name="long_description" id="long_description" rows="10">{{ $task->long_description }}</textarea>
             @error('long_description')
                 <p class="error-message">{{ $message }}</p>
             @enderror
         </div>
 
         <div>
-            <button type="submit">Add Task</button>
+            <button type="submit">Update Task</button>
         </div>
     </form>
 @endsection
