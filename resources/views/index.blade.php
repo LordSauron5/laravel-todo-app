c62ba5c644e169394d69da13485746efe276f502
Tyron Aricum <tyronaricum1@gmail.com>
2024-01-14 18:58:12 +0200

    update: add styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,20 +3,20 @@
 @section('title', "The list of tasks")
 
 @section('content')   
-    <div>
-        <a href="{{ route('tasks.create') }}">Create New Task</a>
-    </div>
+    <nav class="mb-4">
+        <a href="{{ route('tasks.create') }}" class="font-medium text-gray-700 underline decoration-pink-500">Create New Task</a>
+    </nav>
 
     @forelse ($tasks as $task)
         <div>
-            <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
+            <a href="{{ route('tasks.show', ['task' => $task->id]) }}" @class(['line-through' => $task->completed]) >{{ $task->title }}</a>
         </div>
     @empty
         no tasks
     @endforelse
 
     @if ($tasks->count())
-    <nav>
+    <nav class="mt-4">
         {{ $tasks->links() }}
     </nav>
     @endif
